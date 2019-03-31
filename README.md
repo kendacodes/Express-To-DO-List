283deebe6e57d8b01442b289b444c2992980d018
KendaCodes <K.Lamandier@gmail.com>
2019-03-31 15:49:07 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# My To Do List
+- This project was  super fun to create. From choosing the color scheme to doing the CSS! It allows users to add items to the list and on a click of the button you can either clear
+Link to project: coming soon
+
+![alt tag](media/ToDoRead.jpg)
+
+How It's Made:
+Tech used: HTML, CSS, JavaScript, Framework of choice
+
+Lessons Learned:
+I learned that gifs are not the easiest to use as a background especially on a simple design.
---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+
+var db, collection;
+
+const url = "mongodb+srv://list:list@cluster0-nnu1u.mongodb.net/test?retryWrites=true";
+const dbName = "list";
+
+app.listen(3000, () => {
+    MongoClient.connect(url, (error, client) => {
+        if(error) {
+            throw error;
+        }
+        db = client.db(dbName);
+        console.log("Connected to `" + dbName + "`!");
+    });
+});
+
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  //console.log(db)
+  db.collection('messages').find().toArray((err, result) => {
+    if (err) return console.log(err)
+    res.render('index.ejs', {messages: result})
+  })
+})
+
+app.post('/messages', (req, res) => {
+  db.collection('messages').save({ msg: req.body.msg, }, (err, result) => {
+    if (err) return console.log(err)
+    console.log('saved to database')
+    res.redirect('/')
+  })
+})
+
+app.put('/messages', (req, res) => {
+  db.collection('messages')
+  .findOneAndUpdate({ msg: req.body.msg}, {
+    $set: {
+
+    }
+  }, {
+    sort: {_id: -1},
+    upsert: true
+  }, (err, result) => {
+    if (err) return res.send(err)
+    res.send(result)
+  })
+})
+
+app.delete('/messages', (req, res) => {
+  db.collection('messages').findOneAndDelete({ msg: req.body.msg}, (err, result) => {
+    if (err) return res.send(500, err)
+    res.send('Message deleted!')
+  })
+})
